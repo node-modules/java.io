8655ad6321cb66d57a1edb4cc03b5a6d72e6adc6
fool2fish <fool2fish@gmail.com>
2014-07-30 20:22:32 +0800

    Pass all object tests.

---
+++ test/fixtures/in/SerialTestRef.js
@@ -0,0 +1,9 @@
+module.exports = { '$': { a: 'a', c: 'a' },
+  '$class':
+   { name: 'test.SerialTestRef',
+     serialVersionUID: '-4067857340094112173',
+     flags: 2,
+     fields:
+      [ { type: 'L', name: 'a', classname: 'Ljava/lang/String;' },
+        { type: 'L', name: 'c', classname: 'Ljava/lang/String;' } ],
+     superClass: null } }
