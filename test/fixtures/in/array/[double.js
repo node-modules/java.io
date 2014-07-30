117075c427a84ab3d8a1f0432b1b665033b1da06
fool2fish <fool2fish@gmail.com>
2014-07-30 14:26:43 +0800

    Pass all array tests.

---
+++ test/fixtures/in/array/[double.js
@@ -0,0 +1,7 @@
+module.exports = { '$': [ 0, 1.1, 2.2, 3.3333 ],
+  '$class':
+   { name: '[D',
+     serialVersionUID: '4514449696888150558',
+     flags: 2,
+     fields: [],
+     superClass: null } }
---
+++ test/fixtures/in/enum/WeekDayEnum_Sun.js
@@ -0,0 +1,12 @@
+module.exports = { '$': { name: 'Sun' },
+  '$class':
+   { name: 'test.WeekDayEnum',
+     serialVersionUID: '0',
+     flags: 18,
+     fields: [],
+     superClass:
+      { name: 'java.lang.Enum',
+        serialVersionUID: '0',
+        flags: 18,
+        fields: [],
+        superClass: null } } }
